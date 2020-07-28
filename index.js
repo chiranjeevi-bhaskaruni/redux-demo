79a88bc1fa231a5861a393d4bc655e7347416182
chiranjeevi-bhaskaruni <bhaskarunichiranjeevi@gmail.com>
2020-07-28 06:09:03 -0500

    Simple Redux demo  - Initial State  - Sample Action  - Basic Reducer  - Create Store with Reducer  - Dispatch Action  - Subscribe to store  - Unsubscribe

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const redux = require('redux');
+const createStore = redux.createStore
+
 console.log('Redux Demo');
+
+const ADD_DATA = 'ADD_DATA';
+
+const AddAction = {
+    type: ADD_DATA,
+    desc: 'Adding data'
+}
+
+const initialState = {
+    numOfRecords: 9
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'ADD_DATA': 
+            return {
+                ...state,
+                numOfRecords: state.numOfRecords + 1
+            }
+        default: 
+            return state
+    }
+}
+
+const store = createStore(reducer);
+console.log(store.getState());
+const unsubscribe = store.subscribe(() => console.log('State Changes', store.getState()));
+store.dispatch(AddAction);
+store.dispatch(AddAction);
+store.dispatch(AddAction);
+unsubscribe();
+store.dispatch(AddAction);
+
