8f8c3bf2649b8c353629069aaf8af64b152f9e95
chiranjeevi-bhaskaruni <bhaskarunichiranjeevi@gmail.com>
2020-07-28 06:54:54 -0500

    Multiple Reducers  - Seperate stores  - Seperate Actions  - Seperate Dispatchers  - Combine Reducers

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const redux = require('redux');
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 console.log('Redux Demo');
 
 const ADD_DATA = 'ADD_DATA';
+const FIRST_DATA = 'FIRST_DATA';
+const SECOND_DATA = 'SECOND_DATA';
 
 const AddAction = {
     type: ADD_DATA,
     desc: 'Adding data'
 }
 
+const FirstAction = {
+    type: FIRST_DATA,
+    desc: 'First data'
+}
+
+const SecondAction = {
+    type: SECOND_DATA,
+    desc: 'Second data'
+}
+
 const initialState = {
     numOfRecords: 9
+}
+
+const firstInitialData = {
+    data: 7
+}
+
+const secondInitialData = {
+    data: 9
 }
 
 const reducer = (state = initialState, action) => {
@@ -26,12 +47,46 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer);
+const firstReducer = (state = firstInitialData, action) => {
+    switch(action.type) {
+        case FIRST_DATA: 
+            return {
+                ...state,
+                data: state.data + 1
+            }
+        default: 
+            return state
+    }
+}
+
+const secondReducer = (state = secondInitialData, action) => {
+    switch(action.type) {
+        case SECOND_DATA: 
+            return {
+                ...state,
+                data: state.data - 1
+            }
+        default: 
+            return state
+    }
+}
+
+// const store = createStore(reducer);
+// console.log(store.getState());
+// const unsubscribe = store.subscribe(() => console.log('State Changes', store.getState()));
+// store.dispatch(AddAction);
+// store.dispatch(AddAction);
+// store.dispatch(AddAction);
+// unsubscribe();
+// store.dispatch(AddAction);
+
+const rootReducer = combineReducers( {firstReducer, secondReducer});
+const store = createStore(rootReducer);
 console.log(store.getState());
 const unsubscribe = store.subscribe(() => console.log('State Changes', store.getState()));
-store.dispatch(AddAction);
-store.dispatch(AddAction);
-store.dispatch(AddAction);
+store.dispatch(FirstAction);
+store.dispatch(SecondAction);
+store.dispatch(FirstAction);
+store.dispatch(SecondAction);
 unsubscribe();
-store.dispatch(AddAction);
 
